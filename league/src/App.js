d48dc882d898ba65c00ff9e83d1e8ade340f3097
atlfinesse <finesse.cap@gmail.com>
2021-01-07 12:53:44 -0800

    Created attack buttons

--- league/src/App.js
+++ league/src/App.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import { Admin, Unit, blueUnit, redUnit, yellowUnit } from './Objects/Unit'
+import { Admin, Unit, blueUnit, redUnit, yellowUnit } from './Classes/Unit'
 
 function App() {
 	// console.log(Unit)
 	// console.log(Admin)
-	console.log(blueUnit);
-	console.log(yellowUnit);
+	// console.log(blueUnit);
+	// console.log(yellowUnit);
 
 	const handleClick = () => {
     yellowUnit.attackUnit(redUnit);
     console.log(redUnit)
-	};
+  };
+  
+  const attack = (yellowUnit, blueUnit) => {
+    console.log(yellowUnit)
+    console.log(blueUnit)
+  }
 
 	return (
 		<div className='App'>
 			Wassup Planet
-			<button onClick={handleClick}>attack</button>
+			<button onClick={handleClick}>blue attack red</button>
+			<button onClick={handleClick}>red attack blue</button>
+			<button onClick={() => attack(yellowUnit, blueUnit)}>yellow attack blue</button>
 		</div>
 	);
 }
