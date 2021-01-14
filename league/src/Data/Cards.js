d1aed8cb8b1d3c52ad5b5f3990a8e307452b666e
atlfinesse <finesse.cap@gmail.com>
2021-01-13 22:01:14 -0800

    Fixed bug from undeclared "involved"

--- league/src/Data/Cards.js
+++ league/src/Data/Cards.js
@@ -4,12 +4,31 @@ class Card {
         this.type = type
     }
 
-    effect(initiator, target) {
-        console.log('NOOOOOOOOO', initiator, target)
-    }
+
 }
 
-const test = new Card('test', 'attack')
+class AttackCard extends Card {
+	constructor(name, type) {
+		super(name);
+		this.type = type;
+	}
+	effect(initiator, target) {
+        if (initiator !== target) {
+            target.health -= initiator.attack;
+            console.log(
+                `card ${this.name} was used by ${initiator.name} to attack ${target.name} and now it has ${target.health} health remaining`
+                );
+                if (target.health <= 0) {
+                    target.isAlive = false;
+                    console.log(`${target.name} died from the attack`);
+                }
+            } else {
+                console.log('CHOOSE NEW TARGET');
+            }
+	}
+}
+
+const test = new AttackCard('test', 'attack')
 
 
 export const allCards = [test]
--- league/src/Pages/GameBoard/GameBoard.js
+++ league/src/Pages/GameBoard/GameBoard.js
@@ -26,62 +26,61 @@ const GameBoard = ({
 			console.log(a, 'was card used 1');
 			// set initiator to where the button was clicked
 			// set action to what was clicked
-            setInvolved({ ...involved, card: a, action: a.type });
+			setInvolved({ ...involved, card: a, action: a.type });
 		},
 
 		// initiate this action once select is opened
 		initiate: function initiate(b) {
 			// 2 initiate
-			// do stuff only if button was clicked
-			if (involved.action && !involved.initiator) {
-				// set selected target to the next click
-				setInvolved({ ...involved, initiator: b });
-				// depending on the action do something
-            }
-            console.log(b, 'was object that initiated 2')
-        },
-        
-        choose: function choose(c) {
-            // setInvolved({ ...involved, selectedTarget: c})
-            // perform this action using the selected target
-            if (involved.initiator && involved.card) {
-                if (involved.card.type === 'attack') {
-                    this.attack(involved.initiator, c);
+            // do stuff only if button was clicked
+                if (involved.action && !involved.initiator) {
+                    // set selected target to the next click
+                    setInvolved({ ...involved, initiator: b });
+                    // depending on the action do something
                 }
-                console.log('card effect', involved.card.effect(involved.initiator, c))
-            }
+                console.log(b, 'was object that initiated 2');
         },
-        
+
+		// 3 ACTION for cards
+		choose: function choose(c) {
+			// setInvolved({ ...involved, selectedTarget: c})
+			// perform this action using the selected target
+			if (involved.initiator && involved.card) {
+				if (involved.card.type === 'attack') {
+					involved.card.effect(involved.initiator, c)
+				}
+				this.check(playerTeam);
+				this.check(playerKingdoms);
+				this.check(enemyUnits);
+				// set action to falsey so only this click is registered
+				setInvolved(false);
+			}
+		},
+
 		// actions to take
 		// attacker is where the first button was clicked (involved.initiator)
 		// target is the second click (involved.selectedTarget)
 		attack: function attack(attacker, target) {
-            // 3 ACTION
+			// 3 ACTION for enemies
 			if (attacker !== target) {
-                if (attacker.isFaang === true) {
-                    // do something else
-                } else {
-                    attacker.attackUnit(target);
-                    console.log('it works')
-                }
-                this.check(playerTeam)
-                this.check(playerKingdoms)
-                this.check(enemyUnits)
+				if (attacker.isFaang === true) {
+					// do something else
+				} else {
+					attacker.attackUnit(target);
+					console.log('it works');
+				}
 			} else {
-				console.log('CHOOSE NEW TARGET');
-            }
-            // set action in involved to false so only this click is registered
-            setInvolved();
-        },
-        
-        check: function check(array) {
-            for (let i = 0; i < array.length; i++) {
-                if (array[i].isAlive === false) {
-                    array.splice(i, 1)
-                    console.log(`${array[i].name} was spliced at:`, i)
-                }
-            }
-        }
+			}
+		},
+
+		check: function check(array) {
+			for (let i = 0; i < array.length; i++) {
+				if (array[i].isAlive === false) {
+					array.splice(i, 1);
+					console.log(`${array[i].name} was spliced at:`, i);
+				}
+			}
+		},
 	};
 
 	return (
