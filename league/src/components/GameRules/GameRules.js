d5c6ab7d10f529f9963d0a4781aca7b28b8267e5
atlfinesse <finesse.cap@gmail.com>
2021-02-17 20:30:48 -0800

    Added basic styling to rules

--- league/src/components/GameRules/GameRules.js
+++ league/src/components/GameRules/GameRules.js
@@ -1,48 +1,71 @@
 import React, { useEffect, useRef } from 'react';
-import { GameRulesWrapper, RulesDiv, RulesH3, RulesHeading, RulesLI, RulesOL, RulesP } from './SCGameRules';
+import {
+	GameRulesWrapper,
+	LoseDiv,
+	RulesDiv,
+	RulesFlexContainer,
+	RulesH3,
+	RulesHeading,
+	RulesLI,
+	RulesOL,
+	RulesP,
+	WinDiv
+} from './SCGameRules';
 
-const GameRules = ({ showRules, setShowRules}) => {
-	const panel = useRef()
+const GameRules = ({ showRules, setShowRules }) => {
+	const panel = useRef();
 
 	const handleClick = (e) => {
 		if (!panel.current.contains(e.target)) {
-			setShowRules(!showRules)
+			setShowRules(!showRules);
 		}
-	}
+	};
 
 	useEffect(() => {
-		document.addEventListener('mousedown', handleClick)
+		document.addEventListener('mousedown', handleClick);
 
-		return() => {
-			document.removeEventListener('mousedown', handleClick)
-		}
-	}, [])
+		return () => {
+			document.removeEventListener('mousedown', handleClick);
+		};
+	}, []);
 
 	return (
 		<GameRulesWrapper ref={panel}>
 			<RulesHeading>Rules</RulesHeading>
+			<RulesFlexContainer>
+				<WinDiv>
+					<RulesH3>Win</RulesH3>
+					<RulesP>Defeat all the Faangs</RulesP>
+				</WinDiv>
+				<LoseDiv>
+					<RulesH3>Lose</RulesH3>
+					<RulesP>Lose all of your titans</RulesP>
+					<RulesP>Lose all of your kingdoms</RulesP>
+				</LoseDiv>
+			</RulesFlexContainer>
+
 			<RulesDiv>
-
-			<RulesH3>Win</RulesH3>
-			<RulesP>Defeat all the Faangs</RulesP>
-			</RulesDiv>
-			<RulesDiv>
-
-			<RulesH3>Lose</RulesH3>
-			<RulesP>Lose all of your titans</RulesP>
-			<RulesP>Lose all of your kingdoms</RulesP>
-			</RulesDiv>
-
-			<RulesDiv>
-
-            <RulesH3>Turns</RulesH3>
-            <RulesOL>
-                <RulesLI>New random cards are added to the players' hand from the deck</RulesLI>
-                <RulesLI>Swap titans between active ones and reserves (optional)</RulesLI>
-                <RulesLI>Select the card, the titan to use it, and the target</RulesLI>
-                <RulesLI>After <button>End Turn</button> button is clicked all of the units take their turns according to their speed</RulesLI>
-                <RulesLI>If units are defeated, they are replaced after the damage step</RulesLI>
-            </RulesOL>
+				<RulesH3>Turns</RulesH3>
+				<RulesOL>
+					<RulesLI>
+						New random cards are added to the players' hand from the
+						deck
+					</RulesLI>
+					<RulesLI>
+						Swap titans between active ones and reserves (optional)
+					</RulesLI>
+					<RulesLI>
+						Select the card, the titan to use it, and the target
+					</RulesLI>
+					<RulesLI>
+						After <button>End Turn</button> button is clicked all of
+						the units take their turns according to their speed
+					</RulesLI>
+					<RulesLI>
+						If units are defeated, they are replaced after the
+						damage step
+					</RulesLI>
+				</RulesOL>
 			</RulesDiv>
 		</GameRulesWrapper>
 	);
--- league/src/components/GameRules/SCGameRules.js
+++ league/src/components/GameRules/SCGameRules.js
@@ -1,13 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const GameRulesWrapper = styled.div`
+	position: absolute;
+	top: 25%;
+	left: 25%;
 	background-color: white;
-	margin: auto;
-    padding: 30px;
-	width: 100%;
-	height: 100%;
-	width: 70vh;
-	height: 70vh;
+	padding: 30px;
 	z-index: 2;
 	position: fixed;
 	border-radius: 10px;
@@ -16,19 +14,38 @@ export const GameRulesWrapper = styled.div`
 		rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `;
 
-export const RulesDiv = styled.div`
+export const RulesFlexContainer = styled.div`
+	display: flex;
+	justify-content: space-around;
 	padding: 5px;
 	margin: 10px;
-`
+`;
+
+export const RulesDiv = styled.div`
+	padding: 10px;
+	margin: 10px;
+	border-radius: 10px;
+`;
+
+export const WinDiv = styled(RulesDiv)`
+	background-color: #66ff66;
+`;
+
+export const LoseDiv = styled(RulesDiv)`
+	background-color: #ff4d4d;
+`;
 
 export const RulesHeading = styled.h1`
 	text-align: center;
-`
+`;
 
-export const RulesH3 = styled.h3``
+export const RulesH3 = styled.h3`
+	text-align: center;
+	margin: 5px;
+`;
 
-export const RulesP = styled.p``
+export const RulesP = styled.p``;
 
-export const RulesOL = styled.ol``
+export const RulesOL = styled.ol``;
 
-export const RulesLI = styled.li``
+export const RulesLI = styled.li``;
