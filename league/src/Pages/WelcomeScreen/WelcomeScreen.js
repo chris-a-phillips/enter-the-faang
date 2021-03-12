465b668ab3fcd3ea2d4a2669bd81a40b3875d969
atlfinesse <finesse.cap@gmail.com>
2021-03-11 21:21:38 -0800

    Setting up chooseOrder function for welcome screen

--- league/src/Pages/WelcomeScreen/WelcomeScreen.js
+++ league/src/Pages/WelcomeScreen/WelcomeScreen.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function WelcomeScreen ({ setDifficulty, setArmySize, setGameStarted, enemyUnits, setTrueSkill, playerTeam, setPlayerTeam }) {
-	
-    return (
+function WelcomeScreen({
+	setDifficulty,
+	setArmySize,
+	setGameStarted,
+	enemyUnits,
+	setTrueSkill,
+	playerTeam,
+	setPlayerTeam,
+}) {
+	const [starters, setStarters] = useState([]);
+	const [swapPlaces, setSwapPlaces] = useState({
+		state: false,
+		index: null,
+	});
+
+	function chooseOrder(list, activeOne, activeTwo) {
+		if (starters.length === 0) {
+			setStarters();
+		}
+
+		if (swapPlaces.state && starters.length === 2) {
+		}
+	}
+
+	// function swap(list, activeOne, activeTwo) {
+	// 	if (swapPlaces.state === true) {
+	// 		list[activeOne] = list.splice(activeTwo, 1, list[activeOne])[0];
+	// 		setActiveTitans(playerTeam.slice(0, 2));
+	// 	}
+	// 	setSwapPlaces({
+	// 		state: false,
+	// 		index: null,
+	// 	});
+	// 	setAllUnitsOnField(listUnits());
+	// }
+
+	return (
 		<div>
 			<div>
 				Choose Difficulty(ratio of difficult and easy enemies)
@@ -23,7 +57,8 @@ function WelcomeScreen ({ setDifficulty, setArmySize, setGameStarted, enemyUnits
 			<br />
 
 			<div>
-				Choose True Skill (sliders for stats and how tough the combat is)
+				Choose True Skill (sliders for stats and how tough the combat
+				is)
 				<button onClick={() => setTrueSkill('normal')}>normal</button>
 				<button onClick={() => setTrueSkill('heroic')}>heroic</button>
 				<button onClick={() => setTrueSkill('unfair')}>unfair</button>
@@ -33,10 +68,24 @@ function WelcomeScreen ({ setDifficulty, setArmySize, setGameStarted, enemyUnits
 			<h1>Choose Your Starting Units</h1>
 			{playerTeam.map((titan) => {
 				return (
-					<div>
+					<div onClick={chooseOrder}>
 						{titan.name}
+						<button
+							onClick={() => {
+								setSwapPlaces({
+									state: true,
+									index: playerTeam.indexOf(titan),
+								});
+								chooseOrder(
+									playerTeam,
+									swapPlaces.index,
+									playerTeam.indexOf(titan)
+								);
+							}}>
+							+
+						</button>
 					</div>
-				)
+				);
 			})}
 
 			{enemyUnits ? (
@@ -44,6 +93,6 @@ function WelcomeScreen ({ setDifficulty, setArmySize, setGameStarted, enemyUnits
 			) : null}
 		</div>
 	);
-};
+}
 
 export default WelcomeScreen;
